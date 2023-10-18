4e1fe29e00feef62b94de6858e4c8bb24f5f3242
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-10-18 16:42:39 +0545

    [ADD]navbar added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
-// import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import './Home.css'
 
 const Home = () => {
+    // class to className
+    // img tag should be self closing
   return (
-    <div>Home</div>
-  )
+    <>
+    <Navbar/>
+    <div className="card">
+        <img src="product-image.jpg" alt="Product Image" />
+        <h2 className="product-name">Product Name</h2>
+        <p className="product-description">This is a brief product description. It can be longer if needed.</p>
+    </div> 
+    </>
+     )
 }
 
 export default Home
