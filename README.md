fe8bcdc67e508d1aa2e9e87403992063623cc090
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-10-18 17:27:53 +0545

    [ADD] fetchProducts arrow function to get products, .map for loop in products array, key in div for loop

--- README.md
+++ README.md
@@ -2,4 +2,33 @@ Product Management System
 
 1. npm create vite@latest >> project name >> package name >> React >> JavaScript
 2. npm install react-router-dom
-3.
+3. create new folder under src>>pages>>Home
+4. Home.jsx, Home.css
+// class to className
+// img tag should be self closing
+5. add routes in App.jsx
+    <BrowserRouter>
+    <Routes>
+      <Route>
+    </Routes>
+    </BrowserRouter>
+    import {BrowserRouter,Routes,Route} from 'react-router-dom'
+6. add in App.jsx
+      <Route path="/" element={<Home/>}/>
+7. create new folder under src>>components>>Navbar // since Navbar will be used in all pages so folder name is components
+8. Navbar.jsx, Navbar.css
+9. add 
+<Navbar/> in Home.jsx
+10.
+https://mockapi.io/
+JSON for data store, transport, readable by any language so json format used.
+11. npm install axios //3rd party
+12. Home.jsx
+a.useEffect() // do something
+b. fetchProducts arrow function asynchronous
+c. use axios to get data from api made in mockapi.io
+d. useState
+// data array ma ako xa so [] in useState // object ma ako vaye {} hunthyo
+e. data in products array now use .map to loop in products array and show data
+added key in the div
+f. 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
+import axios from 'axios'
 
 const Home = () => {
+    const [products, setProducts]=useState([]) // data array ma ako xa so [] in useState // object ma ako vaye {} hunthyo
+    const fetchProducts = async ()=>{
+        const response = await axios.get("https://652fbacd6c756603295d8ec5.mockapi.io/products")
+        // console.log("Hello Response",response);
+        setProducts(response.data)
+    }
+
+    useEffect(()=>{
+        //do something
+        fetchProducts()
+    },[])
+    // console.log("products",products);
+
     // class to className
     // img tag should be self closing
   return (
     <>
     <Navbar/>
-    <div className="card">
-        <img src="product-image.jpg" alt="Product Image" />
-        <h2 className="product-name">Product Name</h2>
-        <p className="product-description">This is a brief product description. It can be longer if needed.</p>
-    </div> 
+    {
+        products.map((product)=>{
+            return(
+                <div key={product.id} className="card">
+                <img src={product.productImage} alt="Product Image" />
+                <h2 className="product-name">{product.productName}</h2>
+                <p className="product-description">{product.productDescription}</p>
+            </div> 
+            )
+        })
+    }
+
     </>
      )
 }
