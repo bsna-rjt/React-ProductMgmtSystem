6c884b31f2d7f37f7c9b81bc7bbd4973663318cd
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-10-29 17:09:06 +0545

    2nd approach - name of input field, FormData

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -10,14 +10,26 @@ const [productName,setProductName]=useState("")
 const [productDescription,setProductDescription]=useState("")
 const [productMaterial,setProductMaterial]=useState("")
 
-const addProduct = (e)=>{
+const addProduct = async (e)=>{
     e.preventDefault();
-    axios.post("https://652fbacd6c756603295d8ec5.mockapi.io/products",{
-        productImage:productImage,
-        productNAme:productName,
-        productDescription:productDescription,
-        productMaterial:productMaterial
-    })
+        //FIRST APPROACH
+
+    // const response = await axios.post("https://652fbacd6c756603295d8ec5.mockapi.io/products",{
+    //     productImage:productImage,
+    //     productNAme:productName,
+    //     productDescription:productDescription,
+    //     productMaterial:productMaterial
+    // })
+
+    //SECOND APPROACH
+    const formData= new FormData(e.currentTarget);
+    console.log(formData.get('productNAme'));
+    console.log(...formData);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const response = await axios.post("https://652fbacd6c756603295d8ec5.mockapi.io/products",data)
+    console.log("response",response);
     
 }
 
@@ -28,7 +40,7 @@ console.log(productImage,productName,productDescription,productMaterial);
   <div id="product-form">
     <h2>Product Information</h2>
     <form onSubmit={addProduct}>
-      <label htmlFor="productImage">Product Image:</label>
+      {/* <label htmlFor="productImage">Product Image:</label>
       <input type="text" id="productImage" name="productImage" onChange={(e)=>setProductImage(e.target.value)}/>
       
       <label htmlFor="productName">Product Name:</label>
@@ -38,7 +50,19 @@ console.log(productImage,productName,productDescription,productMaterial);
       <textarea id="productDescription" name="productDescription" rows="4" onChange={(e)=>setProductDescription(e.target.value)}></textarea>
       
       <label htmlFor="productMaterial">Product Material:</label>
-      <input type="text" id="productMaterial" name="productMaterial" onChange={(e)=>setProductMaterial(e.target.value)}/>
+      <input type="text" id="productMaterial" name="productMaterial" onChange={(e)=>setProductMaterial(e.target.value)}/> */}
+
+<label htmlFor="productImage">Product Image:</label>
+      <input type="text" id="productImage" name="productImage" />
+      
+      <label htmlFor="productName">Product Name:</label>
+      <input type="text" id="productName" name="productName" />
+      
+      <label htmlFor="productDescription">Product Description:</label>
+      <textarea id="productDescription" name="productDescription" rows="4" ></textarea>
+      
+      <label htmlFor="productMaterial">Product Material:</label>
+      <input type="text" id="productMaterial" name="productMaterial" />
       
       <button type="submit">Submit</button>
     </form>
