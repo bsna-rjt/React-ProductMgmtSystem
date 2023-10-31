eee9ea539d99020eda599bc57263dd6e701f4e9d
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-10-31 17:12:19 +0545

    edit product

--- src/pages/EditProduct/EditProduct.jsx
+++ src/pages/EditProduct/EditProduct.jsx
@@ -1,87 +1,97 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import Navbar from '../../components/Navbar/Navbar';
-import './EditProduct.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useNavigate, useParams } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import "./EditProduct.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const EditProduct = () => {
-    const navigate = useNavigate()
-    const {id} = useParams();
-console.log("id",id);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  // console.log("id",id);
+  const [product, setProduct] = useState({});
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
+    console.log(name, value);
     setProduct({
       ...product,
       [name]: value,
     });
-    console.log("product", product);
+  };
+  // console.log("product",product)
+
+  // edit product
+  const editProduct = async (e) => {
+    e.preventDefault();
+    const response = await axios.put(
+      "https://652fbacd6c756603295d8ec5.mockapi.io/products/" + id,
+      product
+    );
+    if (response.status == 200) {
+      navigate("/singleProduct/" + id);
+    } else {
+      alert("Something went wrong . Try again");
+    }
   };
 
+  // fetch product of id
+  const fetchProduct = async () => {
+    const response = await axios.get(
+      "https://652fbacd6c756603295d8ec5.mockapi.io/products/" + id
+    );
+    setProduct(response.data);
+  };
 
-const[product,setProduct]=useState({});
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div id="product-form">
+        <h2>Edit Product</h2>
+        <form onSubmit={editProduct}>
+          <label htmlFor="productImage">Product Image:</label>
 
-//edit
-const editProduct=async ()=>{
-    const response = await axios.put("https://652fbacd6c756603295d8ec5.mockapi.io/products/"+id,product)
-    console.log("response",response)
-    if(response.status == 200){
-        navigate("/");
-    }
-}
-    //fetch single product
-    const fetchProduct = async ()=>{
-        const response = await axios.get("https://652fbacd6c756603295d8ec5.mockapi.io/products/"+id)
-        console.log("response",response)
-        setProduct(response.data);
-    }
+          <input
+            type="text"
+            value={product.productImage}
+            id="productImage"
+            name="productImage"
+            onChange={handleChange}
+          />
 
-    useEffect(()=>{
-        fetchProduct();
-    })
-        return (
-        <>
-          <Navbar />
-          <div id="product-form">
-            <h2>Edit Product</h2>
-            <form onSubmit={editProduct}>
-              <label htmlFor="productImage">Product Image:</label>
-              <input value={product.productImage}
-                type="text"
-                id="productImage"
-                name="productImage"
-                onChange={handleChange}
-              />
-    
-              <label htmlFor="productName">Product Name:</label>
-              <input value={product.productNAme}
-                type="text"
-                id="productName"
-                name="productName"
-                onChange={handleChange}
-              />
-    
-              <label htmlFor="productDescription">Product Description:</label>
-              <textarea value={product.productDescription}
-                id="productDescription"
-                name="productDescription"
-                rows="4"
-                onChange={handleChange}
-              ></textarea>
-    
-              <label htmlFor="productMaterial">Product Material:</label>
-              <input value={product.productMaterial}
-                type="text"
-                id="productMaterial"
-                name="productMaterial"
-                onChange={handleChange}
-              />
-    
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-        </>
-      );
-}
+          <label htmlFor="productName">Product Name:</label>
+          <input
+            value={product.productNAme}
+            type="text"
+            id="productName"
+            name="productNAme"
+            onChange={handleChange}
+          />
 
-export default EditProduct
+          <label htmlFor="productDescription">Product Description:</label>
+          <textarea
+            value={product.productDescription}
+            id="productDescription"
+            name="productDescription"
+            rows="4"
+            onChange={handleChange}
+          ></textarea>
+
+          <label htmlFor="productMaterial">Product Material:</label>
+          <input
+            value={product.productMaterial}
+            type="text"
+            id="productMaterial"
+            name="productMaterial"
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default EditProduct;
